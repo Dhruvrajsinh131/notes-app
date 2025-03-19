aad0c018a80b38e010fdb3c7dc7b871e21c5c9d3
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2025-03-19 23:03:50 +0530

    edit content

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface CardDataProps {
   title: String;
@@ -8,24 +8,58 @@ interface CardDataProps {
 
 const Card = (props: CardDataProps) => {
   const { title, content, status } = props;
+  const [isEditable, setisEditable] = useState(false);
+  const [cardData, setCardData] = useState({
+    title: title,
+    content: content,
+    status: status,
+  });
 
   const bgStatusColor = status === "P" ? "#FFA500" : "#28A745";
 
+  const updateNote = async () => {};
+
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm w-[33%]">
+    <div className="max-w-sm p-6 bg-white text-ellipsis overflow-hidden text-nowrap border border-gray-200 rounded-lg shadow-sm w-[33%] ">
       <div>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
           {title}
         </h5>
       </div>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {content}
-      </p>
+      {isEditable ? (
+        <textarea
+          defaultValue={content}
+          onChange={(e) => {
+            setCardData((prev) => ({ ...prev, content: e.target.value }));
+          }}
+        />
+      ) : (
+        <p
+          title={content || ""}
+          className="mb-3 font-normal text-gray-700 text-ellipsis overflow-hidden text-nowrap dark:text-gray-400"
+        >
+          {content}
+        </p>
+      )}
       <div
         className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[${bgStatusColor}] rounded-lg cursor-pointer `}
       >
         {status}
       </div>
+
+      <button
+        onClick={async () => {
+          console.log("cardData", cardData);
+
+          setisEditable(!isEditable);
+
+          if (isEditable) {
+            await updateNote();
+          }
+        }}
+      >
+        {!isEditable ? `Edit` : `Save`}
+      </button>
     </div>
   );
 };
